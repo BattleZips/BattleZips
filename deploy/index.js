976a44946805ffcaae80c9506dd148bd54510379
Jack Gilcrest <me@jp4g.llc>
2021-12-23 19:20:06 -0500

    remove

--- deploy/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Deploy Verifier
- */
-module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
-    await deploy('Verifier', { from: deployer, log: true });
-};
-module.exports.tags = ['Verifier'];
